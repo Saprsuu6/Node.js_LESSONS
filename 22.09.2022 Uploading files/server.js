d2e2cd6d63d85445e1ea712777f36d7ce241940e
Andry Saprigin <coffeei.2002@gamil.com>
2022-10-17 20:01:24 +0300

    Filters

--- 22.09.2022 Uploading files/server.js
+++ 22.09.2022 Uploading files/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
-const multer  = require("multer"); // похватывает загруженые файлы при помощи enctype="multipart/form-data"
-  
-const app = express(); // фрейм ворк который использует http
-  
-app.use(express.static(__dirname)); // использование директории для обнаружения всех необходимых документов
-app.use(multer({dest:"uploads"}).single("filedata")); // dest - дериктория для загрузки файла single - будет передан один файл
-app.post("/upload", function (req, res) {
-   
-    let filedata = req.file;
-    console.log(filedata);
+const multer = require("multer"); // похватывает загруженые файлы при помощи enctype="multipart/form-data"
 
-    if(!filedata)
-        res.send("Ошибка при загрузке файла");
-    else
-        res.send("Файл загружен");
+const app = express(); // фрейм ворк который использует http
+
+app.use(express.static(__dirname)); // использование директории для обнаружения всех необходимых документов
+app.use(multer({ dest: "uploads" }).single("filedata")); // dest - дериктория для загрузки файла single - будет передан один файл
+app.post("/upload", function (req, res) {
+  let filedata = req.file;
+  console.log(filedata);
+
+  if (!filedata) res.send("Ошибка при загрузке файла");
+  else res.send("Файл загружен");
 });
 app.listen(3000);
